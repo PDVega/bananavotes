80f76ed2c7e2a362c3f5c7e10a031f0fdda02547
pdvega <pdvegaa@gmail.com>
2017-06-15 21:16:03 +0700

    update topic page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const models = require('../models')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express'});
+  // const topics=[];
+  // topics.push({title:'jim', description:'test'}, {title:'john', description:'test 2'})
+  models.Topic.findAll({
+    where:{}
+  })
+  .then((topicList) => {
+    console.log(topicList);
+    res.render('index', { title: 'List Topic', topics:topicList });
+  })
 });
 
 router.get('/login', function(req, res, next) {
