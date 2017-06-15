ee673dccee744edf1582f8af4e6cd958b00ef3e8
pdvega <pdvegaa@gmail.com>
2017-06-15 22:54:24 +0700

    insert to database

--- routes/topics.js
+++ routes/topics.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
-const db = require ('../models');
+const models = require ('../models');
 
 /* GET home page. */
-router.get('/add', function(req, res, next) {
-  res.render('add_topic', { title: 'Add Topic' });
+router.post('/add', function(req, res, next) {
+  let title_topic = req.body.title;
+  let desc_topic = req.body.description;
+  models.Topic.findOne({where:{}})
+  .then(topic => {
+    models.Topic.create({
+      title: title_topic,
+      description: desc_topic
+    })
+    .then(() => {
+      res.redirect('/topics/add')
+    })
+  })
+})
+  
+router.get('/add', function(req, res, next){
+  res.render('add_topic', { title: 'Add Topic' });    
 });
 
 router.get('/vote', function(req, res, next) {
