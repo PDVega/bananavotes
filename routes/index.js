a2ce5b841572aa58a361a83a291cd91662e99871
yusufhacktiv8 <myyusuf1911@gmail.com>
2017-06-15 20:30:40 +0700

    Prepare login

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const models = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +8,14 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login');
+  let showError = false;
+  if (req.query.error !== undefined) {
+    showError = true;
+  }
+
+  // console.log('------->', req.query.error);
+
+  res.render('login', { showError });
 });
 
 router.get('/register', function(req, res, next){
@@ -15,11 +23,42 @@ router.get('/register', function(req, res, next){
 })
 
 router.post('/login', function(req, res, next) {
-  res.redirect('/');
+  const loginForm = req.body;
+  models.Beever.findOne({
+    where: {
+      email: loginForm.email,
+      password: loginForm.password,
+    },
+  })
+  .then((beever) => {
+    // console.log(beever);
+    if (beever) {
+      req.session.currentUser = {
+        email: beever.email,
+        name: beever.name,
+      };
+      res.redirect('/');
+    } else {
+      res.redirect('/login?error');
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
 });
 
 router.post('/register', function(req, res, next) {
-  res.redirect('/login');
+  console.log(req.body);
+  const beever = req.body;
+  models.Beever.create(beever)
+  .then((newBeever) => {
+    res.redirect('/login');
+  })
+  .catch((err) => {
+
+  })
 });
 
 router.get('/logout', function(req, res, next) {
